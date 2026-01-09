00d356f2352b4f6cdefac3eca9fb77b07a87e4ab
angheloamir <angheloamir@gmail.com>
2026-01-09 21:59:12 +0800

    minor change in the frontend

--- apps/web/src/pages/Workspace.tsx
+++ apps/web/src/pages/Workspace.tsx
@@ -58,7 +58,7 @@ export default function Workspace(props: WorkspaceProps) {
 function WorkspaceCard(props: WorkspaceItem) {
     return (
         <div className="bg-gray-800 border border-gray-700 overflow-hidden flex flex-col">
-            <header className="p-3 flex-1 flex gap-4">
+            <header className="p-2 flex-1 flex gap-4">
                 <i className={`${props.info.fontawesomeIcon ?? 'fa fa-cube'} text-blue-400 text-3xl`}></i>
                 <div className="flex flex-col h-10 overflow-hidden">
                     <h3 className="text-base font-bold text-white leading-tight mb-0.5 ">
@@ -77,25 +77,25 @@ function WorkspaceCard(props: WorkspaceItem) {
 
                 <div className="flex-1 flex gap-2">
                     {props.isRunningAs == 'start' && (
-                        <button className="flex-1 py-2 px-3 rounded-lg bg-orange-600 text-white hover:bg-orange-500 transition-colors shadow-lg shadow-orange-600/20 text-sm font-medium flex items-center justify-center gap-2 animate-fade-in">
+                        <button className="flex-1 py-1 px-2 rounded-lg bg-orange-600 text-white hover:bg-orange-500 transition-colors shadow-lg shadow-orange-600/20 text-sm font-medium flex items-center justify-center gap-2 animate-fade-in">
                             <i className="fas fa-check-circle text-lg"></i>
                             Server is up
                         </button>
                     )}
                     {props.isRunningAs == 'dev' && (
-                        <button className="flex-1 py-2 px-3 rounded-lg bg-red-600 text-white hover:bg-red-500 transition-colors shadow-lg shadow-red-600/20 text-sm font-medium flex items-center justify-center gap-2 animate-fade-in">
+                        <button className="flex-1 py-1 px-2 rounded-lg bg-red-600 text-white hover:bg-red-500 transition-colors shadow-lg shadow-red-600/20 text-sm font-medium flex items-center justify-center gap-2 animate-fade-in">
                             <i className="fas fa-stop text-lg"></i>
                             Stop Dev
                         </button>
                     )}
                     {props.isRunningAs != 'dev' && props.isRunningAs != 'start' && props.info.startCommand && (
-                        <button className="flex-1 py-2 px-3 rounded-lg bg-green-600 text-white hover:bg-green-500 transition-colors shadow-lg shadow-green-600/20 text-sm font-medium flex items-center justify-center gap-2 animate-fade-in">
+                        <button className="flex-1 py-1 px-2 rounded-lg bg-green-600 text-white hover:bg-green-500 transition-colors shadow-lg shadow-green-600/20 text-sm font-medium flex items-center justify-center gap-2 animate-fade-in">
                             <i className="fas fa-play text-lg"></i>
                             Start
                         </button>
                     )}
                     {props.isRunningAs != 'dev' && props.isRunningAs != 'start' && props.info.devCommand && (
-                        <button className="flex-1 py-2 px-3 rounded-lg bg-blue-600 text-white hover:bg-blue-500 transition-colors shadow-lg shadow-blue-600/20 text-sm font-medium flex items-center justify-center gap-2 animate-fade-in">
+                        <button className="flex-1 py-1 px-2 rounded-lg bg-blue-600 text-white hover:bg-blue-500 transition-colors shadow-lg shadow-blue-600/20 text-sm font-medium flex items-center justify-center gap-2 animate-fade-in">
                             <i className="fas fa-play text-lg"></i>
                             Start Dev
                         </button>
