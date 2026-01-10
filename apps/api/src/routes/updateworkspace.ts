c84dabf67f9fffb3c8fc69398dccdd798eb0929d
angheloamir <angheloamir@gmail.com>
2026-01-11 01:51:20 +0800

    added updating workspace

--- apps/api/src/routes/updateworkspace.ts
+++ apps/api/src/routes/updateworkspace.ts
@@ -18,17 +18,19 @@ router.post("/", async (req: Request, res: Response) => {
         if (!fs.existsSync(packageJsonPath)) {
             res.status(404).send({ error: "package.json not found in workspace path" });
             return;
-        }
+        } 
 
         const packageJson = await fs.readJson(packageJsonPath);
         if (workspace.name) packageJson.name = workspace.name;
-        if (workspace.description) packageJson.description = workspace.description;
-        if (workspace.devCommand)   packageJson.scripts.dev   = workspace.devCommand;
-        if (workspace.startCommand) packageJson.scripts.start = workspace.startCommand;
-        if (workspace.buildCommand) packageJson.scripts.build = workspace.buildCommand;
-        if (workspace.testCommand)  packageJson.scripts.test  = workspace.testCommand;
-        if (workspace.lintCommand)  packageJson.scripts.lint  = workspace.lintCommand;
-        
+        if (workspace.description   != typeof null) packageJson.description = workspace.description;
+        if (workspace.devCommand    != typeof null) packageJson.scripts.dev = workspace.devCommand;
+        if (workspace.startCommand  != typeof null) packageJson.scripts.start = workspace.startCommand;
+        if (workspace.buildCommand  != typeof null) packageJson.scripts.build = workspace.buildCommand;
+        if (workspace.testCommand   != typeof null) packageJson.scripts.test = workspace.testCommand;
+        if (workspace.lintCommand   != typeof null) packageJson.scripts.lint = workspace.lintCommand;
+        if (workspace.stopCommand   != typeof null) packageJson.scripts.stop  = workspace.stopCommand;
+        if (workspace.cleanCommand  != typeof null) packageJson.scripts.clean = workspace.cleanCommand;
+
         await fs.writeJson(packageJsonPath, packageJson, { spaces: 2 });
         res.send({ success: true, message: "Workspace updated successfully" });
     } catch (error: any) {
