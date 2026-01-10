3788727aaa9d8261f6e6cf79b93095ce18f7f861
angheloamir <angheloamir@gmail.com>
2026-01-11 02:58:23 +0800

    fix a bug on the modalbody

--- apps/web/src/components/workspace/ModalBody.tsx
+++ apps/web/src/components/workspace/ModalBody.tsx
@@ -1,6 +1,6 @@
 export default function ModalBody( { children }: { children: React.ReactNode }) {
     return (
-        <div onClick={close} className="fixed inset-0 z-[9999] flex items-center justify-center bg-gray-900/70">
+        <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-gray-900/70">
             <div onClick={(e) => e.stopPropagation()} className="bg-gray-800 border border-gray-700 w-[80%] md:w-[60%] max-w-[650px] h-[80%] max-h-[650x] overflow-hidden flex flex-col">
                 {children}
             </div>
