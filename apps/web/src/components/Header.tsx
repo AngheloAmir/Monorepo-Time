545d0152b2bf436dd0a7f551b141cd50cd99293c
angheloamir <angheloamir@gmail.com>
2026-02-12 17:28:06 +0800

    header size changes

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -6,12 +6,12 @@ export default function Header() {
     const setShowGit = useAppState.use.setShowGit();
 
     return (
-        <header className="h-12 w-full px-6 py-2 sticky top-0">
+        <header className="h-7 w-full px-6 py-2 sticky top-0">
             <div className="flex items-center justify-between gap-4 h-full">
                 <div className="flex items-center gap-2">
                     <Logo />
                     <div>
-                        <h1 className="text-xl font-space tracking-tight text-white font-medium">
+                        <h1 className="text-md font-space tracking-tight text-white font-medium">
                             Monorepo
                             <span className="font-bold pl-2 text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-400">
                                 Time
@@ -20,10 +20,10 @@ export default function Header() {
                     </div>
                 </div>
 
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 pt-2">
                     <button
                         onClick={() => setShowGit(true)}
-                        className="flex items-center gap-2 py-1 px-2 rounded text-gray-300 hover:text-white hover:bg-white/10 transition-all group"
+                        className="flex items-center gap-2 px-2 rounded text-gray-300 hover:text-white hover:bg-white/10 transition-all group"
                     >
                         <i className="text-2xl fa-brands fa-git-alt text-blue-400 group-hover:text-blue-300"></i>
                         <span className="text-sm">Press 
--- apps/web/src/components/Logo.tsx
+++ apps/web/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
-        <div className="flex items-center justify-center w-8 h-8 bg-gradient-to-br from-blue-600 to-blue-400 rounded-lg">
-            <img src="/logo.svg" alt="Logo" className="w-7 h-7" />
+        <div className="flex items-center justify-center w-5 h-5 bg-gradient-to-br from-blue-600 to-blue-400 rounded-lg">
+            <img src="/logo.svg" alt="Logo" className="w-5 h-5" />
         </div>
     );
 }
